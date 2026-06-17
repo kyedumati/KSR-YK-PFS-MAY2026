2a7be3b050fd4619d044144cb2a034c5a0e690ef
kyedumati <53014791+kyedumati@users.noreply.github.com>
2026-06-17 11:10:31 +0530

    advanced datatype class1

---
+++ TypeCasting_str.py
@@ -0,0 +1,11 @@
+# str()
+a = 200
+print(type(a))
+a = str(a)
+print(type(a))
+float_value = 123.456
+print(type(float_value))
+print(type(str(float_value)))
+
+print(str(10+5j))
+print(str(True))
---
+++ datatype_list.py
@@ -0,0 +1,5 @@
+#list
+cricketers_list = ["vaibhav", "rohit", "virat", "gill"]
+print(cricketers_list)
+print(type(cricketers_list))
+print(cricketers_list[2])
