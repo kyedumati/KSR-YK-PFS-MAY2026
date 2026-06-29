9b431c6f14091e12b4b05e3d6d35f42c4095011e
kasiy <kasi.yedumati@gmail.com>
2026-06-29 13:02:44 +0530

    input and output

---
+++ input_cmd_line_args.py
@@ -0,0 +1,13 @@
+# command line arguments
+# Write a program to take 2 values from command line arguments, display them and perform sum
+from sys import argv
+
+print("Number of arguments: ", len(argv))
+print("Argument List: ", argv)
+# print(argv[0])
+# print(argv[1])
+# print(argv[2])
+# print(int(argv[1]) + int(argv[2]))
+
+print("Name: ", argv[1])
+# print("Value: ", argv[2])
--- input_output_statements.py
+++ input_output_statements.py
@@ -8,12 +8,38 @@
 # print(a+b)
 
 # wreite a program to read employee data from keyboard and print that data
-empno= int(input("Enter a employee number: "))
-ename= input("Enter a employee name: ")
-eaddr= input("Enter a employee address: ")
-married= input("Enter a married status: ")
-salary= float(input("Enter a salary: "))
-print("Employee Name: ", ename)
-print("Employee Address: ", eaddr)
-print("Married Status: ", married)
-print("Salary: ", salary)
+# empno= int(input("Enter a employee number: "))
+# ename= input("Enter a employee name: ")
+# eaddr= input("Enter a employee address: ")
+# married= input("Enter a married status: ")
+# salary= float(input("Enter a salary: "))
+# print("Employee Name: ", ename)
+# print("Employee Address: ", eaddr)
+# print("Married Status: ", married)
+# print("Salary: ", salary)
+
+# reading multiple values at a time
+# split() is the function in string, which will split values based on delimeter/saperator, if we dont specify any saperator
+# by default it will saperate based on space(" ") and it will return splitted values in list type
+# a = input("Enter 2 numbers in a space saperated:").split()
+# print(type(a))
+# print(a)
+# print(int(a[0]) + int(a[1]))
+# split_string = "kasi is a full stack trainer at KSR".split("kasi")
+# print(split_string)
+# a = input("Enter 3 float numbers in a comma saperated:").split(",")
+# print(type(a))
+# print(a)
+# print(float(a[0]) + float(a[1]))
+
+# eval()
+
+# math_expression = input("Enter a math expression:")
+# print(type(math_expression))
+# print(math_expression)
+# print(eval(math_expression))
+
+# l = eval(input("Enter list of values"))
+# print(list(l))
+# print(type(l))
+
